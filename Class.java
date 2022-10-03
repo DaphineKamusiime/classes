9c8643703f4b402e42a021f59fd62cc2827f5a4b
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-03 11:21:57 -0700

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,10 @@
+public class Class {
+    static void student(String name, int age,String course){
+        System.out.println("Name: "+name+"Age: "+ age+ "   Course:"+ course);
+    }
+    public static void main(String[] args){
+        student("DAPHINE   ",20,"  BSCS");
+        student("Tom       ", 22,"  BSIT");
+        student("Joakim    ",21,"  LLB");
+    }
+}
---
+++ Classes.java
@@ -0,0 +1,10 @@
+public class Classes{
+    int a=9;
+    int b=78;
+    public static void main(String[] args){
+        Classes obj1= new Classes();
+        Classes obj2= new Classes();
+        System.out.println(obj1.a);
+        System.out.println(obj2.b);
+    }
+}
